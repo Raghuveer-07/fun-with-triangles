3062bebd1b22137f4e830299b8c4644da01a9b33
Raghuveer-07 <110120267+Raghuveer-07@users.noreply.github.com>
2022-09-06 03:10:17 +0530

    updated

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -1,16 +1,21 @@
 const baseInput = document.querySelector(".input-base");
-const heightInput= document.querySelector(".input-height");
+const heightInput = document.querySelector(".input-height");
 const checkBtn = document.querySelector("#calculate-area-btn");
 const outputEl = document.querySelector("#output");
 
-checkBtn.addEventListener("click",() => {
+checkBtn.addEventListener("click", () => {
     const base = baseInput.value;
     const height = heightInput.value;
+    if (base > 0 && height > 0) {
+        const areaOfTriangle = 0.5 * base * height;
 
-    const areaOfTriangle = 0.5 * base * height;
+        console.log(areaOfTriangle);
 
-    console.log(areaOfTriangle);
+        outputEl.innerText = "Area of the Triangle is : " + areaOfTriangle + " sq cm.";
+    }
+    else{
+        outputEl.innerText = "Enter all the fields  with value > 0.";
+    }
 
-    outputEl.innerText = "Area of the Triangle is : " + areaOfTriangle + ".";
 
 });
